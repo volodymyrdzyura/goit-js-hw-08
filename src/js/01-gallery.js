2f01e5ed5a6f0fe05b891649ec4de73fc1079a97
Volodymyr Dzyura <103021185+volodymyrdzyura@users.noreply.github.com>
2022-09-06 09:07:46 +0300

    06.09.2022
    правив ./ при підключенні js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Описан в документации
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
 
